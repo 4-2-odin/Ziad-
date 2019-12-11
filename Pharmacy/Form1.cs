bc48d787d0b84419d8b52548a73b485699d3ae18
Ziad <Ziad@DESKTOP-P8RGLGD>
2019-12-11 14:55:24 +0200

    Add project files.

---
+++ Pharmacy/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pharmacy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LogIn_Click(object sender, EventArgs e)
+        {
+            if (username.Text == "kkk" && password.Text == "777")
+            {
+                Form2 f = new Form2();
+                f.Show();
+                this.Hide();
+
+
+            }
+            else
+            {
+                MessageBox.Show("  Invalid username or password");
+                username.Text = "";
+                password.Text = "";
+
+
+            }
+        }
+    }
+}
